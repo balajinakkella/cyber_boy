823988470b0b8e35ef59535f699402efec060207
Balaji Nakkella <kumar.balaji03@gmail.com>
2014-11-18 11:28:13 +0530

    Create checkPair.cpp
    Given an array A[] and a number x, check for pair in A[] with sum as x

---
+++ checkPair.cpp
@@ -0,0 +1,21 @@
+/*
+NOTE:The array should be sorted
+*/
+
+
+bool checkPair(int a[],int n,int x)
+{
+    int l,r;
+    l=0;
+    r=n-1;
+    while(r>l)
+    {
+        if(a[l]+a[r]==x)
+            return true;
+        else if(a[l]+a[r]<x)
+            l++;
+        else 
+            r--;
+    }
+    return false;
+}
